861ac945f4a8c2d3d0023bd3877679a4d5742c71
Vladimir Budilov <odessitv@gmail.com>
2017-02-20 13:22:19 -0500

    Automated the creation of most of the resources

---
+++ setup/cognito-quickstart/createResources.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+ROOT_NAME=$1
+# Bucket name must be all lowercase, and start/end with lowecase letter or number
+# $(echo...) code to work with versions of bash older than 4.0
+BUCKET_NAME=budilov-$(echo "$ROOT_NAME" | tr '[:upper:]' '[:lower:]')
+TABLE_NAME=LoginTrail$ROOT_NAME
+
+# Replace with your 12-digit AWS account ID (e.g., 123456789012)
+AWS_ACCOUNT=$2
+ROLE_NAME_PREFIX=$ROOT_NAME
+POOL_NAME=$ROOT_NAME
+IDENTITY_POOL_NAME=$ROOT_NAME
+REGION=$3
+
+# Create the bucket
+aws s3 mb s3://$BUCKET_NAME/ --region $REGION
+# Add the ‘website’ configuration and bucket policy
+aws s3 website s3://$BUCKET_NAME/ --index-document index.html --error-document index.html
+cat s3-bucket-policy.json | sed 's/BUCKET_NAME/'$BUCKET_NAME'/' > /tmp/s3-bucket-policy.json
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file:///tmp/s3-bucket-policy.json
+#Build the project and sync it up to the bucket
+
+# Create DDB Table
+aws dynamodb create-table \
+    --table-name $TABLE_NAME \
+    --attribute-definitions \
+        AttributeName=userId,AttributeType=S \
+        AttributeName=activityDate,AttributeType=S \
+    --key-schema AttributeName=userId,KeyType=HASH AttributeName=activityDate,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --region $REGION
+
+
+# Create a Cognito Identity and Set roles
+aws cognito-identity create-identity-pool --identity-pool-name $IDENTITY_POOL_NAME --allow-unauthenticated-identities --region $REGION| grep IdentityPoolId | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//' > /tmp/poolId
+identityPoolId=$(cat /tmp/poolId)
+
+# Create an IAM role for unauthenticated users
+cat unauthrole-trust-policy.json | sed 's/IDENTITY_POOL/'$identityPoolId'/' > /tmp/unauthrole-trust-policy.json
+aws iam create-role --role-name $ROLE_NAME_PREFIX-unauthenticated-role --assume-role-policy-document file:///tmp/unauthrole-trust-policy.json
+aws iam put-role-policy --role-name $ROLE_NAME_PREFIX-unauthenticated-role --policy-name CognitoPolicy --policy-document file://unauthrole.json
+
+# Create an IAM role for authenticated users
+cat authrole-trust-policy.json | sed 's/IDENTITY_POOL/'$identityPoolId'/' > /tmp/authrole-trust-policy.json
+aws iam create-role --role-name $ROLE_NAME_PREFIX-authenticated-role --assume-role-policy-document file:///tmp/authrole-trust-policy.json
+cat authrole.json | sed 's/TABLE_NAME/'$TABLE_NAME'/' | sed 's/ACCOUNT_NUMBER/'$AWS_ACCOUNT'/' | sed 's/REGION/'$REGION'/' > /tmp/authrole.json
+aws iam put-role-policy --role-name $ROLE_NAME_PREFIX-authenticated-role --policy-name CognitoPolicy --policy-document file:///tmp/authrole.json
+
+# Create the user pool
+aws cognito-idp create-user-pool --pool-name $POOL_NAME --auto-verified-attributes email --schema Name=email,Required=true --policies file://user-pool-policy.json --region $REGION > /tmp/$POOL_NAME-create-user-pool
+userPoolId=$(grep -E '"Id":' /tmp/$POOL_NAME-create-user-pool | awk -F'"' '{print $4}')
+
+# Create the user pool client
+aws cognito-idp create-user-pool-client --user-pool-id $userPoolId --no-generate-secret --client-name webapp --region $REGION > /tmp/$POOL_NAME-create-user-pool-client
+userPoolClientId=$(grep -E '"ClientId":' /tmp/$POOL_NAME-create-user-pool-client | awk -F'"' '{print $4}')
+
+# Add the user pool and user pool client id to the identity pool
+aws cognito-identity update-identity-pool --allow-unauthenticated-identities --identity-pool-id $identityPoolId --identity-pool-name $IDENTITY_POOL_NAME --cognito-identity-providers ProviderName=cognito-idp.$REGION.amazonaws.com/$userPoolId,ClientId=$userPoolClientId --region $REGION
+
+# Update cognito identity with the roles
+# If this command gives you an error, associate the roles manually
+aws cognito-identity set-identity-pool-roles --identity-pool-id $identityPoolId --roles authenticated=arn:aws:iam::$AWS_ACCOUNT:role/$ROLE_NAME_PREFIX-authenticated-role,unauthenticated=arn:aws:iam::$AWS_ACCOUNT:role/$ROLE_NAME_PREFIX-unauthenticated-role --region $REGION
+
+echo $userPoolId > /tmp/userPoolId
---
+++ setup/setupEnvironment.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+ROOT_NAME=$(date +%Y%m%d%H%M%S)
+echo "Using the following 'root name' for your resources, such as S3 bucket: " ${ROOT_NAME}
+
+BUCKET_NAME=${ROOT_NAME}-rekognition
+REGION=us-east-1
+ES_DOMAIN_NAME=rekognition # if you change this value, you'll need to change the values in the elasticsearch_service_policy.json file
+EXTERNAL_IP_ADDRESS=$(curl ipinfo.io/ip)
+ACCOUNT_NUMBER=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text)
+COGNITO_POOL_NAME_REPLACE_ME=${ROOT_NAME}rek
+
+# Lambda
+JAR_LOCATION=../build/libs/rekognition-rest-1.0-SNAPSHOT.jar
+FUNCTION_REK_SEARCH=rekognition-search-picture
+FUNCTION_REK_ADD=rekognition-add-picture
+FUNCTION_REK_DEL=rekognition-del-picture
+FUNCTION_REK_SEARCH_HANDLER=com.budilov.SearchPhotosHandler
+FUNCTION_REK_ADD_HANDLER=com.budilov.AddPhotoLambda
+FUNCTION_REK_DEL_HANDLER=com.budilov.RemovePhotoLambda
+
+# IAM
+ROLE_NAME=s3-to-es
+
+# Methods
+createLambdaFunction() {
+   if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+     echo "No parameters were passed"
+     exit -1
+   fi
+
+    aws lambda create-function --region $1 \
+        --function-name $2 \
+        --zip-file fileb://$3 \
+        --role arn:aws:iam::${ACCOUNT_NUMBER}:role/s3-to-es \
+        --handler $4 \
+        --runtime java8 \
+        --memory-size 192 \
+        --timeout 20
+}
+
+# Setup the Cognito quickstart app and resources
+
+cd ./cognito-quickstart/
+chmod 755 createResources.sh
+./createResources.sh ${COGNITO_POOL_NAME_REPLACE_ME} ${ACCOUNT_NUMBER} ${REGION}
+USER_POOL_ID=$(cat /tmp/userPoolId)
+echo "UserPoolId: " ${USER_POOL_ID}
+cd ..
+
+POOL_ARN_REPLACE_ME=arn:aws:cognito-idp:${REGION}:${ACCOUNT_NUMBER}:userpool/${USER_POOL_ID}
+
+# Create IAM roles
+aws iam create-role --role-name s3-to-es --assume-role-policy-document file://s3-to-es-role-trust-relationship.json
+aws iam attach-role-policy --role-name s3-to-es --policy-arn arn:aws:iam::aws:policy/AmazonESFullAccess
+aws iam attach-role-policy --role-name s3-to-es --policy-arn arn:aws:iam::aws:policy/AmazonRekognitionFullAccess
+aws iam attach-role-policy --role-name s3-to-es --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+
+# Create the photos bucket
+aws s3 mb s3://$BUCKET_NAME/ --region $REGION
+
+# Build and deploy Lambda functions
+## Build
+cd ..
+chmod 755 gradlew
+./gradlew build
+cd setup
+
+## Create and deploy
+createLambdaFunction ${REGION} ${FUNCTION_REK_ADD} ${JAR_LOCATION} ${FUNCTION_REK_ADD_HANDLER} > /tmp/addlambdaoutput
+REKOGNITION_ADD_FUNCTION_ARN=$(grep FunctionArn /tmp/addlambdaoutput | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//')
+
+createLambdaFunction ${REGION} ${FUNCTION_REK_DEL} ${JAR_LOCATION} ${FUNCTION_REK_DEL_HANDLER} > /tmp/dellambdaoutput
+REKOGNITION_DELETE_FUNCTION_ARN=$(grep FunctionArn /tmp/dellambdaoutput | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//')
+
+createLambdaFunction ${REGION} ${FUNCTION_REK_SEARCH} ${JAR_LOCATION} ${FUNCTION_REK_SEARCH_HANDLER} > /tmp/searchlambdaoutput
+REKOGNITION_SEARCH_FUNCTION_ARN=$(grep FunctionArn /tmp/searchlambdaoutput | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//')
+
+# Setup the S3 events
+echo "Setting up the S3 Lambda events"
+
+echo "Adding the ADD event Lambda permissions"
+aws lambda add-permission \
+    --function-name ${FUNCTION_REK_ADD} \
+    --region ${REGION} \
+    --statement-id ${ROOT_NAME}-rekognition \
+    --action "lambda:InvokeFunction" \
+    --principal s3.amazonaws.com \
+    --source-arn arn:aws:s3:::${BUCKET_NAME} \
+    --source-account ${ACCOUNT_NUMBER}
+
+echo "Adding the DEL event Lambda permissions"
+aws lambda add-permission \
+    --function-name ${FUNCTION_REK_DEL} \
+    --region ${REGION} \
+    --statement-id ${ROOT_NAME}-rekognition \
+    --action "lambda:InvokeFunction" \
+    --principal s3.amazonaws.com \
+    --source-arn arn:aws:s3:::${BUCKET_NAME} \
+    --source-account ${ACCOUNT_NUMBER}
+
+echo "Preparing the S3 Notifications json definitions file"
+cat s3-notifications.json |
+    sed 's#REKOGNITION_DEL_FUNCTION_ARN_REPLACE_ME#'${REKOGNITION_DELETE_FUNCTION_ARN}'#g' |
+    sed 's#REKOGNITION_ADD_FUNCTION_ARN_REPLACE_ME#'${REKOGNITION_ADD_FUNCTION_ARN}'#g' > /tmp/s3-notifications.json
+
+echo "Creating the events"
+aws s3api put-bucket-notification-configuration --bucket ${BUCKET_NAME} --notification-configuration file:///tmp/s3-notifications.json
+
+# Import your API Gateway Swagger template. This command can run after the Cognito User Pool is created
+## First substitute values in the swagger file: COGNITO_POOL_NAME_REPLACE_ME,
+## POOL_ARN_REPLACE_ME, REKOGNITION_ADD_FUNCTION, REKOGNITION_DELETE_FUNCTION, REKOGNITION_SEARCH_FUNCTION
+echo "Preparing the swagger template. REGION: " ${REGION} " COGNITO_POOL_NAME: " ${COGNITO_POOL_NAME_REPLACE_ME} " POOL_ARN_REPLACE_ME: " ${POOL_ARN_REPLACE_ME} " REKOGNITION_SEARCH_FUNCTION_ARN: " ${REKOGNITION_SEARCH_FUNCTION_ARN}
+cat apigateway-swagger.json |
+    sed 's#REGION_REPLACE_ME#'${REGION}'#g' |
+    sed 's#COGNITO_POOL_NAME_REPLACE_ME#'${COGNITO_POOL_NAME_REPLACE_ME}'#g' |
+    sed 's#POOL_ARN_REPLACE_ME#'${POOL_ARN_REPLACE_ME}'#g' |
+    sed 's#REKOGNITION_SEARCH_FUNCTION_ARN_REPLACE_ME#'${REKOGNITION_SEARCH_FUNCTION_ARN}'#g' > /tmp/apigateway-swagger.json
+
+echo "Importing the swagger template"
+aws apigateway import-rest-api --body 'file:///tmp/apigateway-swagger.json' --region ${REGION} > /tmp/apigateway-import-api
+GATEWAY_ID=$(cat /tmp/apigateway-import-api | grep id | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//')
+
+## Deploy to 'prd' stage
+echo "Deploying the gateway with id of " ${GATEWAY_ID} " to prd"
+aws apigateway create-deployment --rest-api-id ${GATEWAY_ID} --stage-name prod
