0b91cfdbb187af16a7d839705adda160ee016e07
Vladimir Budilov <odessitv@gmail.com>
2017-03-10 14:19:33 -0500

    Added region declaration for every aws cli call

--- setup/setupEnvironment.sh
+++ setup/setupEnvironment.sh
@@ -55,11 +55,12 @@ createLambdaFunction() {
         --handler $4 \
         --runtime java8 \
         --memory-size 192 \
-        --timeout 20
+        --timeout 20 \
+        --region ${REGION}
 
 cat << EOF >> ${DELETE_SCRIPT}
 echo "Deleting the " $2 " Lambda function"
-aws lambda delete-function --function-name $2
+aws lambda delete-function --function-name $2 --region ${REGION}
 
 EOF
 
@@ -74,7 +75,8 @@ updateFunction() {
     aws lambda update-function-code \
     --region $1 \
     --function-name $2 \
-    --zip-file fileb://$3
+    --zip-file fileb://$3 \
+    --region ${REGION}
 }
 
 # Setup the Cognito quickstart app and resources
@@ -90,33 +92,33 @@ cd ..
 POOL_ARN_REPLACE_ME=arn:aws:cognito-idp:${REGION}:${ACCOUNT_NUMBER}:userpool/${USER_POOL_ID}
 
 # Create IAM roles
-aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file://s3-to-es-role-trust-relationship.json
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonESFullAccess
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonRekognitionFullAccess
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
+aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file://s3-to-es-role-trust-relationship.json --region ${REGION}
+aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonESFullAccess --region ${REGION}
+aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonRekognitionFullAccess --region ${REGION}
+aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --region ${REGION}
+aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --region ${REGION}
 
 # Creating the role & role policy deletion
 cat << EOF >> ${DELETE_SCRIPT}
 echo "Detaching ${ROLE_NAME}'s policies and deleting role"
-aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonESFullAccess
-aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonRekognitionFullAccess
-aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
-aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
-aws iam delete-role --role-name ${ROLE_NAME}
+aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonESFullAccess --region ${REGION}
+aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonRekognitionFullAccess --region ${REGION}
+aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --region ${REGION}
+aws iam detach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --region ${REGION}
+aws iam delete-role --role-name ${ROLE_NAME} --region ${REGION}
 
 EOF
 
 
 # Create the photos bucket
 aws s3 mb s3://${BUCKET_NAME}/ --region ${REGION}
-aws s3api put-bucket-cors --bucket ${BUCKET_NAME} --cors-configuration file://s3-bucket-cors-configuration.json
+aws s3api put-bucket-cors --bucket ${BUCKET_NAME} --cors-configuration file://s3-bucket-cors-configuration.json --region ${REGION}
 
 # Create delete bucket code
 cat << EOF >> ${DELETE_SCRIPT}
 echo "Removing the bucket"
-aws s3 rm s3://${BUCKET_NAME} --recursive
-aws s3 rb s3://${BUCKET_NAME}
+aws s3 rm s3://${BUCKET_NAME} --recursive --region ${REGION}
+aws s3 rb s3://${BUCKET_NAME} --region ${REGION}
 
 EOF
 # Build and deploy Lambda functions
@@ -147,7 +149,8 @@ aws lambda add-permission \
     --action "lambda:InvokeFunction" \
     --principal s3.amazonaws.com \
     --source-arn arn:aws:s3:::${BUCKET_NAME} \
-    --source-account ${ACCOUNT_NUMBER}
+    --source-account ${ACCOUNT_NUMBER} \
+    --region ${REGION}
 
 echo "Adding the DEL event Lambda permissions"
 aws lambda add-permission \
@@ -157,7 +160,8 @@ aws lambda add-permission \
     --action "lambda:InvokeFunction" \
     --principal s3.amazonaws.com \
     --source-arn arn:aws:s3:::${BUCKET_NAME} \
-    --source-account ${ACCOUNT_NUMBER}
+    --source-account ${ACCOUNT_NUMBER} \
+    --region ${REGION}
 
 echo "Preparing the S3 Notifications json definitions file"
 cat s3-notifications.json |
@@ -165,33 +169,33 @@ cat s3-notifications.json |
     sed 's#REKOGNITION_ADD_FUNCTION_ARN_REPLACE_ME#'${REKOGNITION_ADD_FUNCTION_ARN}'#g' > /tmp/s3-notifications.json
 
 echo "Creating the events"
-aws s3api put-bucket-notification-configuration --bucket ${BUCKET_NAME} --notification-configuration file:///tmp/s3-notifications.json
+aws s3api put-bucket-notification-configuration --bucket ${BUCKET_NAME} --notification-configuration file:///tmp/s3-notifications.json --region ${REGION}
 
 # Setup Elasticsearch
 echo "Setup Elasticsearch with a domain of ${ES_DOMAIN_NAME}"
-aws es create-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --elasticsearch-version 5.1 --elasticsearch-cluster-config InstanceType=t2.small.elasticsearch,InstanceCount=1 --ebs-options EBSEnabled=true,VolumeType=standard,VolumeSize=10
+aws es create-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --elasticsearch-version 5.1 --elasticsearch-cluster-config InstanceType=t2.small.elasticsearch,InstanceCount=1 --ebs-options EBSEnabled=true,VolumeType=standard,VolumeSize=10 --region ${REGION}
 cat elasticsearch_service_policy.json |
     sed 's#ACCOUNT_NAME_REPLACE_ME#'${ACCOUNT_NUMBER}'#g' |
     sed 's#REGION_REPLACE_ME#'${REGION}'#g' |
     sed 's#ROLE_NAME_REPLACE_ME#'${ROLE_NAME}'#g' |
     sed 's#ES_DOMAIN_NAME_REPLACE_ME#'${ES_DOMAIN_NAME}'#g' |
     sed 's#EXTERNAL_IP_ADDRESS_REPLACE_ME#'${EXTERNAL_IP_ADDRESS}'#g' > /tmp/elasticsearch_service_policy.json
-aws es update-elasticsearch-domain-config --domain-name ${ES_DOMAIN_NAME} --access-policies file:///tmp/elasticsearch_service_policy.json
+aws es update-elasticsearch-domain-config --domain-name ${ES_DOMAIN_NAME} --access-policies file:///tmp/elasticsearch_service_policy.json --region ${REGION}
 
 ## We need the endpoint url now, but the ES domain is most-likely processing right now. Let's wait until it finishes
-while [ 'None' == $(aws es describe-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --query "DomainStatus.Endpoint" --output text) ];
+while [ 'None' == $(aws es describe-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --query "DomainStatus.Endpoint" --output text --region ${REGION}) ];
 do
     echo "Waiting for the ES domain ${ES_DOMAIN_NAME} to finish processing. Sleeping..."
     sleep 30
 done
 
-ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --query "DomainStatus.Endpoint" --output text)
+ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --query "DomainStatus.Endpoint" --output text --region ${REGION})
 echo "Got the ES endpoint: ${ES_ENDPOINT}"
 
 # Create delete ES domain
 cat << EOF >> ${DELETE_SCRIPT}
 echo "Deleting the ES domain"
-aws es delete-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME}
+aws es delete-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --region ${REGION}
 
 EOF
 
@@ -224,12 +228,12 @@ cat apigateway-swagger.json |
     sed 's#REKOGNITION_SEARCH_FUNCTION_ARN_REPLACE_ME#'${REKOGNITION_SEARCH_FUNCTION_ARN}'#g' > /tmp/apigateway-swagger.json
 
 echo "Importing the swagger template"
-aws apigateway import-rest-api --body 'file:///tmp/apigateway-swagger.json' --region ${REGION} > /tmp/apigateway-import-api
+aws apigateway import-rest-api --body 'file:///tmp/apigateway-swagger.json' --region ${REGION} > /tmp/apigateway-import-api  --region ${REGION}
 GATEWAY_ID=$(cat /tmp/apigateway-import-api | grep id | awk '{print $2}' | xargs |sed -e 's/^"//'  -e 's/"$//' -e 's/,$//')
 
 ## Deploy to 'prd' stage
 echo "Deploying the gateway with id of " ${GATEWAY_ID} " to prd"
-aws apigateway create-deployment --rest-api-id ${GATEWAY_ID} --stage-name prod
+aws apigateway create-deployment --rest-api-id ${GATEWAY_ID} --stage-name prod  --region ${REGION}
 API_GATEWAY_URL="https://${GATEWAY_ID}.execute-api.${REGION}.amazonaws.com/prod"
 
 # Grant the API Gateway access to invoke the Lambda function
@@ -238,7 +242,8 @@ aws lambda add-permission \
 --statement-id apigateway-prod \
 --action lambda:InvokeFunction \
 --principal apigateway.amazonaws.com \
---source-arn "arn:aws:execute-api:${REGION}:${ACCOUNT_NUMBER}:${GATEWAY_ID}/prod/POST/picture/search"
+--source-arn "arn:aws:execute-api:${REGION}:${ACCOUNT_NUMBER}:${GATEWAY_ID}/prod/POST/picture/search" \
+--region ${REGION}
 
 ## Create a script that will remove (most) all of the AWS resources created
 cat << EOF >> ${DELETE_SCRIPT}
@@ -248,15 +253,15 @@ aws apigateway delete-rest-api --rest-api-id ${GATEWAY_ID}
 EOF
 
 ## Enable SRP on the cognito client id
-aws cognito-idp update-user-pool-client --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID}  --explicit-auth-flows ADMIN_NO_SRP_AUTH
+aws cognito-idp update-user-pool-client --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID}  --explicit-auth-flows ADMIN_NO_SRP_AUTH  --region ${REGION}
 
 USERNAME="test@user.com"
 PASSWORD="P@ssword1"
 
-aws cognito-idp sign-up --client-id ${COGNITO_CLIENT_ID} --username ${USERNAME} --password ${PASSWORD} --user-attributes '[ { "Name": "email", "Value": "test@user.com" }, { "Name": "phone_number", "Value": "+12485551212" }]'
+aws cognito-idp sign-up --client-id ${COGNITO_CLIENT_ID} --username ${USERNAME} --password ${PASSWORD} --user-attributes '[ { "Name": "email", "Value": "test@user.com" }, { "Name": "phone_number", "Value": "+12485551212" }]'  --region ${REGION}
 
 ## Confirm user
-aws cognito-idp admin-confirm-sign-up --user-pool-id ${USER_POOL_ID} --username ${USERNAME}
+aws cognito-idp admin-confirm-sign-up --user-pool-id ${USER_POOL_ID} --username ${USERNAME}  --region ${REGION}
 
 ## begin auth flow
 cat << EOF > /tmp/authflow.json
@@ -264,9 +269,9 @@ cat << EOF > /tmp/authflow.json
 EOF
 
 # Running the following command to get the JWT Token
-JWT_ID_TOKEN=$(aws cognito-idp admin-initiate-auth  --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID} --cli-input-json file:///tmp/authflow.json --query AuthenticationResult.IdToken --output text)
+JWT_ID_TOKEN=$(aws cognito-idp admin-initiate-auth  --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID} --cli-input-json file:///tmp/authflow.json --query AuthenticationResult.IdToken --output text --region ${REGION})
 
-COGNITO_IDENTITY_ID=$(aws cognito-identity get-id --identity-pool-id ${IDENTITY_POOL_ID} --logins {\"cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}\":\"${JWT_ID_TOKEN}\"} --query "IdentityId" --output text)
+COGNITO_IDENTITY_ID=$(aws cognito-identity get-id --identity-pool-id ${IDENTITY_POOL_ID} --logins {\"cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}\":\"${JWT_ID_TOKEN}\"} --query "IdentityId" --output text --region ${REGION})
 
 echo ""
 echo "------------"
@@ -284,7 +289,7 @@ echo "-----COGNITO IDs-------"
 echo ""
 echo "-----LAMBDA FUNCTIONS-------"
 echo "Add:              " ${FUNCTION_REK_ADD_HANDLER}
-echo "Remove:           " ${FUNCTION_DEL_ADD_HANDLER}
+echo "Remove:           " ${FUNCTION_REK_DEL_HANDLER}
 echo "Search:           " ${FUNCTION_REK_SEARCH_HANDLER}
 echo "-----LAMBDA FUNCTIONS-------"
 echo ""
@@ -292,20 +297,20 @@ echo "----- Try out the following commands -------"
 echo ""
 echo "-> Upload a picture"
 echo "--------------------"
-echo "aws s3 cp new-york.jpg s3://${BUCKET_NAME}/usercontent/${COGNITO_IDENTITY_ID}/"
+echo "aws s3 cp new-york.jpg s3://${BUCKET_NAME}/usercontent/${COGNITO_IDENTITY_ID}/ --region ${REGION}"
 echo ""
 echo "-> Sample search command"
 echo "You might need to pipe the output to 'native2ascii -encoding UTF-8 -reverse' if you"
 echo "want to copy and paste the signed url in the browser since curl encodes url output"
 echo "-------------------------"
-echo "curl -X POST -H \"Authorization: \$(aws cognito-idp admin-initiate-auth  --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID} --cli-input-json file:///tmp/authflow.json --query AuthenticationResult.IdToken --output text)\" -H \"search-key: building\" -H \"Cache-Control: no-cache\" \"${API_GATEWAY_URL}/picture/search/\""
+echo "curl -X POST -H \"Authorization: \$(aws cognito-idp admin-initiate-auth  --user-pool-id ${USER_POOL_ID} --client-id ${COGNITO_CLIENT_ID} --cli-input-json file:///tmp/authflow.json --query AuthenticationResult.IdToken --output text --region ${REGION})\" -H \"search-key: building\" -H \"Cache-Control: no-cache\" \"${API_GATEWAY_URL}/picture/search/\""
 echo ""
 echo "-> Remove the picture"
 echo "----------------------"
-echo "aws s3 rm s3://${BUCKET_NAME}/usercontent/${COGNITO_IDENTITY_ID}/new-york.jpg"
+echo "aws s3 rm s3://${BUCKET_NAME}/usercontent/${COGNITO_IDENTITY_ID}/new-york.jpg --region ${REGION} "
 echo ""
 echo ""
 echo "Want to redeploy the Search Lambda function? Run the following command"
 echo "-----------------------------------------------------------------------"
-echo "aws lambda update-function-code --region ${REGION} --function-name ${FUNCTION_REK_SEARCH} --zip-file fileb://${JAR_LOCATION}"
+echo "aws lambda update-function-code --region ${REGION} --function-name ${FUNCTION_REK_SEARCH} --zip-file fileb://${JAR_LOCATION} --region ${REGION}"
 echo ""
